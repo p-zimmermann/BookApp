03d70e1a82c527acf505ade0521c35950648e71d
p-zimmermann <123363124+p-zimmermann@users.noreply.github.com>
2023-06-24 12:26:40 +0200

    add register

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,157 @@
 const express = require("express");
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require ('multer'); //library for uploading images
+app.use(cors())
+const bcrypt = require("bcrypt"); //library for hashing the password
+const jwt = require("jsonwebtoken");//library for login
+
+app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`)
 })
+
+//connection to mongodb
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+//create a disk storage for all the image uploads
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null,'uploads')
+    },
+    filename: function(req,file,cb) {
+        cb(null,`${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage });
+
+//profile database mongoose schema
+
+const userData = new mongoose.Schema({
+    userId: String,
+    email: String,
+    password: String,
+    profilePicture: String,
+    username: String,
+    resetPasswordToken: String,
+    resetPasswortExpires: Date, 
+}
+)
+
+const UserData = mongoose.model('UserData', userData)
+
+//post route register
+app.post('/register', upload.single('profilePicture'), async (req, res) => {
+    try {
+      console.log("Received data:", req.body);
+     /*  res.status(201).send("Works fine"); */
+      const { id, email, username } = req.body;
+      const password = req.body.password;
+      if (!id || !email || !username || !password) {
+        return res.status(404).send({message: "Please fill out all fields!"})
+      }
+
+      //check if user already exists
+      const existsUser = await UserData.findOne({email});
+      if (existsUser){
+        return res.status(409).send({message: "User already exists"})
+      }
+      // Hash the password with bcrypt
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+      console.log(`data of hashed password: ${hashedPassword}`);
+  
+      // Create a new user document in the database
+      const newUser = await UserData.create({
+        id: req.body.id,
+        email: req.body.email,
+        username: req.body.username,
+        password: hashedPassword,
+        profilePicture: req.file
+          ? `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
+          : "",
+      });
+  
+      // Generate access token
+      const accessToken = jwt.sign(
+        { id: newUser._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1h' }
+      );
+  
+      // Return the new user document and access token as the response
+      res.status(201).json({newUser, accessToken });
+    } catch (error) {
+        console.error("Error in function_name:", error.message);
+        console.error("Error stack trace:", error.stack);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+      
+  });
+
+  // Check if email or username exists
+app.get("/checkuser", async (req, res) => {
+    const { email, username } = req.query;
+  
+    try {
+      const userWithEmail = email
+        ? await Userdata.findOne({ email: email })
+        : null;
+      const userWithUsername = username
+        ? await Userdata.findOne({ username: username })
+        : null;
+  
+      res.status(200).json({
+        emailExists: Boolean(userWithEmail),
+        usernameExists: Boolean(userWithUsername),
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  //route for login
+  app.post("/login", async (req, res) => {
+    try {
+      const { identifier, password } = req.body;
+  
+      // Find user by email or username
+      const user = await Userdata.findOne({
+        $or: [{ email: identifier }, { username: identifier }],
+      });
+  
+      // If user not found, return an error
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      // Check if the provided password matches the user's password
+      const passwordMatch = user && await bcrypt.compare(password, user.password);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Invalid password" });
+      }
+  
+      // Generate access token
+      const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+  
+      // Return the user document and access token as the response
+      res.status(200).json({ user: user, accessToken: accessToken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
--- frontend/src/components/Startpage/Register.jsx
+++ frontend/src/components/Startpage/Register.jsx
@@ -1,6 +1,65 @@
 import { Box, TextField, Input, InputLabel, Button } from "@mui/material";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { useRef, useState } from "react";
 
 export default function Register() {
+  const formRef = useRef();
+
+  //needed for image upload
+  const [selectedFile, setSelectedFile] = useState(null);
+  // Add a new function to handle file changes
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const [user, setUser] = useState({
+    id: "",
+    email: "",
+    password: "",
+    username: "",
+    profilePicture: "",
+  });
+
+  const handleInputChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+    console.log(event.target.value);
+  };
+
+  //axios request
+  const registerUser = async (e) => {
+    e.preventDefault();
+    console.log(user.email);
+    const userForm = new FormData();
+    userForm.append("id", uuidv4());
+    userForm.append("email", user.email);
+    userForm.append("password", user.password);
+    userForm.append("username", user.username);
+    userForm.append("profilePicture", selectedFile);
+    console.log(userForm);
+    const config = {
+      url: `http://localhost:3001/register`,
+      method: "POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: userForm,
+    };
+    try {
+      const response = await axios(config);
+      console.log(response);
+      if (response.status !== 201) {
+        throw new Error("Failed to post");
+      }
+    } catch (error) {
+      if (error.response.status === 429) {
+        console.log(error.response.data);
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <>
       <Box
@@ -27,47 +86,51 @@ export default function Register() {
             padding: 5,
             width: "50%",
           }}
+          component="form"
+          ref={formRef}
+          onSubmit={registerUser}
         >
-          <TextField
-            fullWidth
-            margin="normal"
-            label="E-Mail"
-            name="mail"
-            variant="outlined"
-            value=""
-            onChange=""
-          />
-          <TextField
-            fullWidth
-            margin="normal"
-            label="Password"
-            name="password"
-            variant="outlined"
-            value=""
-            onChange=""
-          />
-          <TextField
-            fullWidth
-            margin="normal"
-            label="nickname"
-            name="nickname"
-            variant="outlined"
-            value=""
-            onChange=""
-          />
-          <InputLabel htmlFor="profilePicture">
-            Upload your profile picture here
-          </InputLabel>
-          <Input
-            id="profilePicture"
-            name="profilePicture"
-            type="file"
-            accept="image/*"
-            onChange=""
-          />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
-            Register
-          </Button>
+          <Box>
+            <TextField
+              fullWidth
+              margin="normal"
+              label="E-Mail"
+              name="email"
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              label="Password"
+              name="password"
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+            <TextField
+              fullWidth
+              margin="normal"
+              label="username"
+              name="username"
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+            <InputLabel htmlFor="profilePicture">
+              Upload your profile picture here
+            </InputLabel>
+            <Input
+              id="profilePicture"
+              name="profilePicture"
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+            />
+          </Box>
+          <Box>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+              Register
+            </Button>
+          </Box>
           {/* <LoginFields></LoginFields> */}
         </Box>
       </Box>
