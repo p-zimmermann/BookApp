9a193cfd415e8c81a81446d41e3493b0812d6d6c
p-zimmermann <123363124+p-zimmermann@users.noreply.github.com>
2023-07-24 16:13:26 +0200

    edit display of nyt bestseller list

--- frontend/src/components/BookSearch/BookSearch.jsx
+++ frontend/src/components/BookSearch/BookSearch.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import SearchResults from "./SearchResults.jsx";
 
+import ShowBook from "../BookUtils/ShowBook.jsx";
 
 export default function BookSearch() {
   const [books, setBooks] = useState([]);
   const [nytBooks, setNytBooks] = useState([]);
-  const [nytCover, setNytCover] = useState([]);
   const nytimesKey = "3QdQnrcpPLUzdrgAFwoJtsqbLYlOsHWF";
-  const googleBooksKey =  "AIzaSyC_GqwBGc6ICB15i7B_V-oZj0KeWzE5WJQ";
+  const googleBooksKey = "AIzaSyC_GqwBGc6ICB15i7B_V-oZj0KeWzE5WJQ";
 
   const fetchNYTBooks = async () => {
     try {
@@ -18,9 +18,9 @@ export default function BookSearch() {
           nytimesKey
       );
       //console.log(response.data.results);
-     /*  setNytBooks(response.data.results); */
-      console.log(response.data.results)
-      getCover(response.data.results)
+      /*  setNytBooks(response.data.results); */
+      console.log(response.data.results);
+      getCover(response.data.results);
     } catch (error) {
       console.error("Error fetching bestseller books:", error);
     }
@@ -28,51 +28,29 @@ export default function BookSearch() {
   // get nyt bestseller list
   useEffect(() => {
     fetchNYTBooks();
-    
   }, []);
   // get book cover from nyt bestseller list
   async function getCover(nytBooks) {
-    const nytBookCover = nytBooks.map((nytBook)=>{
+    const nytBookCover = nytBooks.map((nytBook) => {
       return axios
-      .get(
-        'https://www.googleapis.com/books/v1/volumes', 
-        {
+        .get("https://www.googleapis.com/books/v1/volumes", {
           params: {
             q: `isbn:${nytBook.isbns[0].isbn13}`,
             key: "AIzaSyC_GqwBGc6ICB15i7B_V-oZj0KeWzE5WJQ",
           },
         })
-      .then((res) => {
-        /*  console.log(res) */
-        console.log(res)
-        setNytCover(nytCover => [...nytCover, res.data.items[0].volumeInfo.imageLinks.thumbnail]);
-        setNytBooks(nytBooks => [...nytBooks, res.data.items[0].volumeInfo.title])
-       })
-       .catch((e) => console.error(e));
+        .then((res) => {
+          /*  console.log(res) */
+          console.log(res)
+          setNytBooks((nytBooks) => [...nytBooks, res.data.items[0]]);
+        })
+        .catch((e) => console.error(e));
     });
-    Promise.all(nytBookCover).then(value => console.log(value))
+    Promise.all(nytBookCover).then((value) => console.log(value));
   }
 
-  /*   nytBooks.map((nytBook) => {
-      try {
-        const response = await axios.get(
-            'https://www.googleapis.com/books/v1/volumes', 
-            {
-              params: {
-                q: `isbn:${nytBook.isbns[0].isbn13}`,
-                key: "AIzaSyC_GqwBGc6ICB15i7B_V-oZj0KeWzE5WJQ",
-              },
-            }
-        );
-        console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail)
-        setNytCover(nytCover => [...nytCover, response.data.items[0].volumeInfo.imageLinks.thumbnail])
-      } catch (error) {
-        console.error("Error fetching book cover:", error);
-    }
-  })
-  } */
   //get userID
-  const loggedUser = JSON.parse(localStorage.getItem("user"))
+  const loggedUser = JSON.parse(localStorage.getItem("user"));
 
   //get book from google api
   const fetchBooks = async (value) => {
@@ -102,16 +80,13 @@ export default function BookSearch() {
   const [searchText, setSearchText] = useState("");
   const handleChange = (event) => {
     setSearchText(event.target.value);
-   
   };
   const onClick = () => {
     fetchBooks(searchText);
   };
   return (
     <>
-      <Box sx={{
-        
-      }}>
+      <Box sx={{}}>
         <TextField
           label="search for title"
           /* value='search' */
@@ -125,7 +100,9 @@ export default function BookSearch() {
         </Button>
       </Box>
 
-      <Typography variant="h4" sx={{my: 3}} >Results</Typography>
+      <Typography variant="h4" sx={{ my: 3 }}>
+        Results
+      </Typography>
       <Box
         sx={{
           display: "flex",
@@ -134,28 +111,39 @@ export default function BookSearch() {
           my: 3,
         }}
       >
-        {books ? (books.map((book, index) => { 
-          return <SearchResults key={index} book={book} />;
-        })) : (
+        {books ? (
+          books.map((book, index) => {
+            return <SearchResults key={index} book={book} />;
+          })
+        ) : (
           <Typography>No Search Results</Typography>
-        )
-      }
+        )}
       </Box>
-      <Typography variant="h4" sx={{my: 3}}>Current NYT Bestseller Books</Typography>
-      <ul>
-        {nytCover.map((bookCover, index) => (
+      <Typography variant="h4" sx={{ my: 3 }}>
+        Current NYT Bestseller Books (not in order)
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          my: 3,
+        }}
+      >
+        {nytBooks.map((nytBooks, index) => (
           <>
-          <li key={bookCover.id}>
-            <Typography>{nytBooks[index]}</Typography>
-          </li>
-            <img src={bookCover}></img>
+            <Box
+              sx={{
+                width: "20%",
+                padding: 2,
+              }}
+              key={nytBooks.id}
+            >
+              <ShowBook book={nytBooks} />
+            </Box>
           </>
-          
-          ))}
-      </ul>
-      {}
-           
+        ))}
+      </Box>
     </>
   );
 }
-
